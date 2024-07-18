dcd3803329371a3be5a27ea7c245fe1f82e6fc17
DanielBrisch <danielbrisch157@gmail.com>
2024-07-17 22:18:43 -0300

    fix: resolvendo estrutura da tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const QRPage(),
+    return const MaterialApp(
+      title: 'MaterCode',
+      home: QRPage(),
+      initialRoute: Routes.login,
       onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -10,6 +10,241 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    bool lembrar = false;
+
+    return Scaffold(
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        return SizedBox(
+          height: constraints.maxHeight,
+          width: constraints.maxWidth,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset(
+                './lib/assets/background.jpeg',
+                fit: BoxFit.cover,
+                width: constraints.maxWidth,
+                height: constraints.maxHeight,
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          'Bem Vindo!',
+                          style: TextStyle(
+                            color: Colors.white,
+                            decoration: TextDecoration.none,
+                            fontSize: 40,
+                          ),
+                        ),
+                        Image.asset(
+                          './lib/assets/acenando.png',
+                          width: 50,
+                          fit: BoxFit.fill,
+                        )
+                      ],
+                    ),
+                    const Text(
+                      'Bem-vindo ao MaterCode!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.none,
+                        fontSize: 25,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    const Text(
+                      'Email',
+                      style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.none,
+                        fontSize: 18,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'email@exemplo.com',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Password',
+                      style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.none,
+                        fontSize: 18,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: '*****',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(
+                              checkColor: Colors.black,
+                              activeColor: Colors.white,
+                              onChanged: (value) {
+                                setState(() {
+                                  lembrar = value!;
+                                });
+                              },
+                              value: lembrar,
+                            ),
+                            const Text(
+                              'Lembrar de mim',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          'Esqueci minha senha',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 250, 154, 197),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: constraints.maxWidth,
+                            height: 50,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor: const WidgetStatePropertyAll(
+                                    Color(0xFF4E0189)),
+                                shape: WidgetStateProperty.all(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            color: Colors.transparent,
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: const Row(
+                              children: [
+                                Expanded(
+                                  child: Divider(
+                                    color: Colors.white,
+                                    thickness: 2,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  child: Text(
+                                    'ou então',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Divider(
+                                    color: Colors.white,
+                                    thickness: 2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'Nao tem uma conta?',
+                                style: TextStyle(
+                                  color: Color(0xFF999EA1),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  'Registre-se agora!',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 250, 154, 197),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 200,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    './lib/assets/googleLogo.png',
+                                    width: 35,
+                                    fit: BoxFit.fill,
+                                  ),
+                                  const Text('Entrar com google')
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
+    );
   }
 }
